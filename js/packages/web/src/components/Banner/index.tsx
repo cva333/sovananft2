6c9f497ebb6bc00a6488b9de9e266625be6b32f8
Claudine Baluyot <a.claudine2398@gmail.com>
2022-08-29 15:01:37 +0800

    change banner ui desktop

--- js/packages/web/src/components/Banner/index.tsx
+++ js/packages/web/src/components/Banner/index.tsx
@@ -41,22 +41,26 @@ export const Banner = (props: {
           {props.actionComponent}
         </div>
       </div>
-      <div
-        id={'current-banner'}
-        style={{ backgroundImage: `url(${props.src})` }}
-      >
-        <span id={'gradient-banner'}></span>
-        <div id="banner-inner">
-          <div id={'message-container'}>
-            <div id={'main-heading'}>{props.headingText}</div>
-            <div id={'sub-heading'}>{props.subHeadingText}</div>
-            {props.actionComponent}
-          </div>
-          {props.children}
-          <div className="powered-by">
-            <span>
-              {/* POWERED BY <b>METAPLEX</b> */}
-            </span>
+      <div id="bannerBg">
+        <div
+          id={'current-banner'}
+          style={{ backgroundImage: `url(${props.src})` }}
+        >
+          <span id={'gradient-banner'}></span>
+          <div id="banner-inner">
+            <div id={'message-container'}>
+              <p className="subs">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                molestie, erat ac commodo facilisis
+              </p>
+              {/* <div id={'main-heading'}>{props.headingText}</div> */}
+              {/* <div id={'sub-heading'}>{props.subHeadingText}</div> */}
+              {/* {props.actionComponent} */}
+            </div>
+            {props.children}
+            <div className="powered-by">
+              <span>{/* POWERED BY <b>METAPLEX</b> */}</span>
+            </div>
           </div>
         </div>
       </div>
