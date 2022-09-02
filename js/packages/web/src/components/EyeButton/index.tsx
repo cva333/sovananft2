bf34a5c7d8e474a58b959e654377701ea9ad3b0a
Claudine Baluyot <a.claudine2398@gmail.com>
2022-09-02 17:33:06 +0800

    add changes

---
+++ js/packages/web/src/components/EyeButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { HeartOutlined } from '@ant-design/icons';
+// import { Button } from 'antd';
+
+export const EyeButton = () => {
+  return (
+    <div className="like-btn">
+      <img src={'/eye-white.png'} />
+    </div>
+  );
+};
