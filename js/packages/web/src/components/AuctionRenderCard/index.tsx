2a893758c1a45a1e3bc04ffb15015d86dd1c1db0
Claudine Baluyot <a.claudine2398@gmail.com>
2022-09-03 19:55:19 +0800

    fix homepage

--- js/packages/web/src/components/AuctionRenderCard/index.tsx
+++ js/packages/web/src/components/AuctionRenderCard/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardProps } from 'antd';
 import { ArtContent } from '../ArtContent';
 import { AuctionView, useArt, useCreators } from '../../hooks';
 import { AmountLabel } from '../AmountLabel';
-import { MetaAvatar } from '../MetaAvatar';
+// import { MetaAvatar } from '../MetaAvatar';
 import { AuctionCountdown } from '../AuctionNumbers';
 import { LoveButton } from '../LoveButton';
 import { SelectButton } from '../SelectButton';
@@ -28,13 +28,15 @@ export const AuctionRenderCard = (props: AuctionCard) => {
     m => m.address == auctionView.auction.info.tokenMint,
   )[0];
   const { status, amount } = useAuctionStatus(auctionView);
+  const [hoverAuction, setHoverAuction] = useState(false);
 
   const card = (
     <Card hoverable={true} className={`auction-render-card`} bordered={false}>
       <div className={'card-art-info'}>
         {/* <div className="auction-gray-wrapper"> */}
         <div className={'card-artist-info'}>
-          <MetaAvatar creators={creators.length ? [creators[0]] : undefined} />
+          {/* <MetaAvatar creators={creators.length ? [creators[0]] : undefined} /> */}
+          <img src="/apeCardLogo.svg" width={45} className="crdLogo" />
           <span className={'artist-name'}>
             {creators[0]?.name ||
               creators[0]?.address?.substr(0, 6) ||
@@ -44,11 +46,32 @@ export const AuctionRenderCard = (props: AuctionCard) => {
           <span className="love-btn">
             <LoveButton />
           </span>
-          <span className="tick-btn">
-            <SelectButton />
-          </span>
+
+          {hoverAuction ? (
+            <div>
+              <span className="tick-btn">
+                <SelectButton />
+              </span>
+              <div className="buycontainer">
+                <button
+                  type="button"
+                  className="ant-btn ant-btn-default connector buyBtn"
+                >
+                  <span>Buy Now</span>
+                </button>
+              </div>
+            </div>
+          ) : (
+            <span />
+          )}
         </div>
-        <div className={'art-content-wrapper'}>
+        <div
+          className={'art-content-wrapper'}
+          onMouseOver={() => setHoverAuction(true)}
+          onFocus={() => setHoverAuction(true)}
+          onMouseOut={() => setHoverAuction(false)}
+          onBlur={() => setHoverAuction(false)}
+        >
           <ArtContent
             className="auction-image no-events"
             preview={false}
