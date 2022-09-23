fb1c24a6a499a37cc190ff572fa66370911a9f3e
Claudine Baluyot <a.claudine2398@gmail.com>
2022-09-23 15:07:18 +0800

    add collection ui

---
+++ js/packages/web/src/components/AuctionRenderCard/indexCollection.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Card, CardProps } from 'antd';
+import { ArtContent } from '../ArtContent';
+import { AuctionView, useArt, useCreators } from '../../hooks';
+import { AmountLabel } from '../AmountLabel';
+// import { MetaAvatar } from '../MetaAvatar';
+import { AuctionCountdown } from '../AuctionNumbers';
+import { LoveButton } from '../LoveButton';
+
+import { useAuctionStatus } from './hooks/useAuctionStatus';
+import { useTokenList } from '../../contexts/tokenList';
+
+// import {heart} from '../../../../../assets/heart-white.svg'
+
+export interface AuctionCollectionCard extends CardProps {
+  auctionView: AuctionView;
+}
+
+export const AuctionRenderCard = (props: AuctionCollectionCard) => {
+  const { auctionView } = props;
+  const id = auctionView.thumbnail.metadata.pubkey;
+  const art = useArt(id);
+  const creators = useCreators(auctionView);
+  const name = art?.title || ' ';
+
+  const tokenInfo = useTokenList().subscribedTokens.filter(
+    m => m.address == auctionView.auction.info.tokenMint,
+  )[0];
+  const { status, amount } = useAuctionStatus(auctionView);
+  const [hoverAuction, setHoverAuction] = useState(false);
+
+  console.log(hoverAuction);
+
+  const card = (
+    <Card
+      hoverable={true}
+      className={`auction-render-card mobiletest`}
+      bordered={false}
+    >
+      <div className={'card-art-info'}>
+        {/* <div className="auction-gray-wrapper"> */}
+        <div className={'card-artist-info'}>
+          {/* <MetaAvatar creators={creators.length ? [creators[0]] : undefined} /> */}
+          <img src="/apeCardLogo.svg" width={45} className="crdLogo" />
+          <span className={'artist-name'}>
+            {creators[0]?.name ||
+              creators[0]?.address?.substr(0, 6) ||
+              'Go to auction'}
+            ...
+          </span>
+          <span className="love-btn">
+            <LoveButton />
+          </span>
+        </div>
+        <div
+          className={'art-content-wrapper'}
+          onMouseOver={() => setHoverAuction(true)}
+          onFocus={() => setHoverAuction(true)}
+          onMouseOut={() => setHoverAuction(false)}
+          onBlur={() => setHoverAuction(false)}
+        >
+          <ArtContent
+            className="auction-image no-events"
+            preview={false}
+            pubkey={id}
+            allowMeshRender={false}
+          />
+          {/* <Button className="secondary-btn">Buy Now</Button> */}
+        </div>
+        <div className={'art-name'}>{name}</div>
+        {!auctionView.isInstantSale && (
+          <div className="auction-info-container">
+            <div className={'info-message'}>ENDING IN</div>
+            <AuctionCountdown auctionView={auctionView} labels={false} />
+          </div>
+        )}
+        {/* </div> */}
+        <div className="card-bid-info">
+          <span className={'text-uppercase info-message'}>{status}</span>
+          <AmountLabel
+            containerStyle={{ flexDirection: 'row' }}
+            amount={amount}
+            iconSize={24}
+            tokenInfo={tokenInfo}
+          />
+        </div>
+      </div>
+    </Card>
+  );
+
+  return card;
+};
--- js/packages/web/src/views/collections/collectionDetail/index.tsx
+++ js/packages/web/src/views/collections/collectionDetail/index.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { Banner } from '../../../components/Banner';
 import { pubkeyToString, useMeta } from '@oyster/common';
 import { useExtendedArt } from '../../../hooks';
-import { SalesListView } from '../../home/components/SalesList';
+import { CollectionListView } from '../../home/components/SalesList/collectionList';
 
 export const CollectionDetailView = () => {
   const { id } = useParams<{ id: string }>();
@@ -13,19 +13,14 @@ export const CollectionDetailView = () => {
     () => metadataByCollection[key].pubkey,
     [key, metadataByCollection],
   );
-  const { ref, data } = useExtendedArt(pubkey);
+  const { ref } = useExtendedArt(pubkey);
 
   return (
     <>
       <div ref={ref} className={'collection-banner'}>
-        <Banner
-          src={data?.image ?? ''}
-          headingText={data?.name ?? ''}
-          subHeadingText={data?.description ?? ''}
-          useBannerBg
-        />
+        <Banner src={''} headingText={''} subHeadingText={''} useBannerBg />
       </div>
-      {/* <SalesListView collectionMintFilter={key} /> */}
+      <CollectionListView collectionMintFilter={key} />
     </>
   );
 };
