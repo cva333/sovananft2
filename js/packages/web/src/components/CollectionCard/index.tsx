adfcc9418fd1f0551b0d120ede6196440e8590e5
Claudine Baluyot <a.claudine2398@gmail.com>
2022-09-23 11:05:18 +0800

    add collection ui

--- js/packages/web/src/components/CollectionCard/index.tsx
+++ js/packages/web/src/components/CollectionCard/index.tsx
@@ -15,15 +15,45 @@ export const CollectionCard = (props: { pubkey: StringPublicKey }) => {
       <div className={'asset-container'}>
         <div className={'art-content-wrapper'}>
           <ArtContent
-            className="auction-image no-events"
+            className="collectionsAuction auction-image no-events"
             preview={false}
             pubkey={pubkey}
             allowMeshRender={false}
           />
         </div>
       </div>
-      <div className={'collection-title'}>{art.title}</div>
-      <div className={'collection-description'}>{data?.description}</div>
+
+      <div className="flexCol">
+        <div>
+          <div className="flexRow">
+            <div className={'collection-description'}>{data?.description}</div>
+            <div className={'collection-title'} style={{ width: '50%' }}>
+              {art.title}
+            </div>
+            <div className="center" style={{ width: '50%' }}>
+              <div className="flexCol">
+                <div>Volume</div>
+                <div>
+                  <img src="/logoSolana.svg" width={24} /> 8.04K
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={'collection-OpenBtn'}>
+            <div className="buycontainerCollection">
+              <button
+                type="button"
+                className="ant-btn ant-btn-default connector buyBtn pb-5"
+              >
+                <span>Open Collection</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className={'collection-artists'}>
         <MetaAvatar creators={art.creators} />
         <span className={'artist-name'}>
